1f24d83fddec99aa5d5c242652f7decfeff93dd3
jonathangomz <jonicgp97@gmail.com>
2021-03-02 13:53:36 -0600

    Initial commit

---
+++ components/Logo/Inner.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    width="163"
+    height="163"
+    viewBox="0 0 163 163"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M120.787 120.786C99.2471 142.326 64.3247 142.326 42.7852 120.786C21.2457 99.2467 21.2457 64.3243 42.7852 42.7848C64.3247 21.2453 99.2471 21.2453 120.787 42.7848"
+      stroke="#86BBD8"
+      stroke-width="4"
+    />
+  </svg>
+</template>
+<style>
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ components/Logo/Logo.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="Logo">
+    <div class="center">
+      <Outer class="outer"/>
+      <Inner class="inner"/>
+      <Diagonal class="diagonal"/>
+    </div>
+  </div>
+</template>
+<style>
+.center {
+  display: inline;
+  position: absolute;
+  z-index: -1;
+  top: 100px;
+  left: 50%;
+  animation: appear 1s;
+}
+
+.center > svg {
+  position: relative;
+  left: -50%;
+}
+
+.center .outer {
+  animation: rotate-reverse 1s;
+}
+
+.center .inner {
+  bottom: 164px;
+  animation: rotate 1s;
+}
+
+.center .diagonal {
+  bottom: 360px;
+  left: -30%;
+  animation: rotate 1s;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes rotate-reverse {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(-360deg);
+  }
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
