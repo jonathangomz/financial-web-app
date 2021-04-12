07538bf5fd78e204b50df66e28f6fac12ab63105
jonathangomz <jonicgp97@gmail.com>
2021-04-12 11:19:44 -0500

    Last commit before delete old pc

--- components/Splash.vue
+++ components/Splash.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="fixed z-10 inset-0 overflow-y-auto">
+  <div id="splash" v-bind:class="{ 'ease-out': easeOut }" v-if="show" class="fixed z-10 inset-0 overflow-y-auto" >
     <div
-      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      class="flex items-center justify-center min-h-screen p-4 text-center sm:block sm:p-0"
     >
       <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        <div class="absolute inset-0 bg-white"></div>
       </div>
 
       <span
@@ -13,13 +13,55 @@
         >&#8203;</span
       >
       <div
-        class="inline-block sm:align-middle sm:max-w-lg sm:w-full relative"
+        class="inline-block transform transition-all align-middle max-w-lg"
+        style="border-radius: 15px;"
         role="dialog"
         aria-modal="true"
         aria-labelledby="modal-headline"
       >
-        <Logo />
+        <div class="p-6">
+          <div class="flex items-center justify-center">
+            <div
+              class="mx-auto flex-shrink-0 flex items-center justify-center rounded-full sm:mx-0 h-full w-full"
+            >
+              <Icon />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<style scoped>
+#splash {
+  opacity: 1;
+}
+
+#splash.ease-out {
+  transition: opacity .8s ease-in-out;
+  opacity: 0;
+}
+</style>
+<script lang="ts">
+import Vue from 'vue';
+
+const show: boolean = true;
+const easeOut: boolean = false;
+
+export default Vue.extend({
+  data: function() {
+    return {
+      show, easeOut,
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.easeOut = true
+    }, 3000);
+
+    setTimeout(() => {
+      this.show = false
+    }, 3800);
+  },
+})
+</script>
